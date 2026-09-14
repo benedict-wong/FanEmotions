15dc0d010625bb81f1d3b46be8f43f9e3e7f7636
Benedict Wong <33556035+benedict-wong@users.noreply.github.com>
2026-09-14 12:58:05 -0700

    Update SearchBar.tsx

--- src/app/components/SearchBar.tsx
+++ src/app/components/SearchBar.tsx
@@ -1,14 +1,16 @@
 'use client'
 import { useState } from 'react'
-import { useRouter } from 'next/navigation'
+import { useRouter, useSearchParams } from 'next/navigation'
 
 interface SearchBarProps {
   placeholder?: string
 }
 
 export default function SearchBar({ placeholder = 'Search...' }: SearchBarProps) {
-  const [search, setSearch] = useState('')
   const router = useRouter()
+  const searchParams = useSearchParams()
+  const query = searchParams.get('q') || ''
+  const [search, setSearch] = useState(query)
 
   function handleSearch(event: React.ChangeEvent<HTMLInputElement>) {
     setSearch(event.target.value)
@@ -18,9 +20,9 @@ export default function SearchBar({ placeholder = 'Search...' }: SearchBarProps)
     // When pressing enter and the search state is not empty, do the following below:
     if (event.key === 'Enter' && search.trim()) {
       // Handle search submission here. Launch the search results page with a search query parameter of the search state.
-      router.push(`/search?query=${encodeURIComponent(search.trim())}`)
+      router.push(`/search?q=${encodeURIComponent(search.trim())}`)
     }
   }
 
-  return <input type="search" placeholder={placeholder} value={search} onChange={handleSearch} onKeyDown={handleEnter} />
+  return <input id="searchbar" type="search" placeholder={placeholder} value={search} onChange={handleSearch} onKeyDown={handleEnter} autoComplete="off" />
 }
