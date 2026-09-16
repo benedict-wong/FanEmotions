07532357291710ba51e2106bd40db5a60c4a1281
Benedict Wong <33556035+benedict-wong@users.noreply.github.com>
2026-09-16 15:40:43 -0700

    add more placeholders for gallery

--- src/app/components/Gallery.tsx
+++ src/app/components/Gallery.tsx
@@ -24,12 +24,62 @@ export default function Gallery() {
       image: '/assets/image/placeholder.jpg',
     },
     {
-      id: 3,
+      id: 4,
       title: 'Passionate',
       image: '/assets/image/placeholder.jpg',
     },
     {
-      id: 3,
+      id: 5,
+      title: 'Amazed',
+      image: '/assets/image/placeholder.jpg',
+    },
+    {
+      id: 6,
+      title: 'Excited',
+      image: '/assets/image/placeholder.jpg',
+    },
+    {
+      id: 7,
+      title: 'Happy',
+      image: '/assets/image/placeholder.jpg',
+    },
+    {
+      id: 8,
+      title: 'Angry',
+      image: '/assets/image/placeholder.jpg',
+    },
+    {
+      id: 9,
+      title: 'Passionate',
+      image: '/assets/image/placeholder.jpg',
+    },
+    {
+      id: 10,
+      title: 'Amazed',
+      image: '/assets/image/placeholder.jpg',
+    },
+    {
+      id: 11,
+      title: 'Excited',
+      image: '/assets/image/placeholder.jpg',
+    },
+    {
+      id: 12,
+      title: 'Happy',
+      image: '/assets/image/placeholder.jpg',
+    },
+    {
+      id: 13,
+      title: 'Angry',
+      image: '/assets/image/placeholder.jpg',
+    },
+    {
+      id: 14,
+      title: 'Passionate',
+      image: '/assets/image/placeholder.jpg',
+    },
+    {
+      id: 15,
       title: 'Amazed',
       image: '/assets/image/placeholder.jpg',
     },
