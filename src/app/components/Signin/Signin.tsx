6685e70cfd575b04acc4c23c209f42767fd97de5
Benedict Wong <33556035+benedict-wong@users.noreply.github.com>
2026-09-16 15:41:06 -0700

    Create Signin.tsx

---
+++ src/app/components/Signin/Signin.tsx
@@ -0,0 +1,14 @@
+import PasswordInput from '../PasswordInput/PasswordInput'
+import styles from './signin.module.scss'
+
+export default function Signin() {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Welcome back!</h1>
+      <form id="signin-form">
+        <input type="email" id="signin-email-input" placeholder="Email"></input>
+        <PasswordInput mode="signin" />
+      </form>
+    </div>
+  )
+}
