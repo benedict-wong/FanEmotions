00792b7544ca17464145bd9b2ccb7cb819e4a0af
Benedict Wong <33556035+benedict-wong@users.noreply.github.com>
2026-09-16 15:43:05 -0700

    create first iteration for register component

---
+++ src/app/components/Register.tsx
@@ -0,0 +1,10 @@
+export default function Register() {
+  return (
+    <form id="register-form">
+      <input id="register-firstName" type="text" placeholder="First Name"></input>
+      <input id="register-lastName" type="text" placeholder="Last Name"></input>
+      <input id="register-email" type="email" placeholder="Email"></input>
+      <input id="register-password" type="password" placeholder="Password"></input>
+    </form>
+  )
+}
