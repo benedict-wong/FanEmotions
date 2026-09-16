4d838a73da523d3e9d94adfb931120b9ff2cfee2
Benedict Wong <33556035+benedict-wong@users.noreply.github.com>
2026-09-16 15:43:28 -0700

    update Header components

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,16 +1,35 @@
+'use client'
+
 import SearchBar from './SearchBar'
 import Link from 'next/link'
+import { useRouter } from 'next/navigation'
+import Logo from '../components/Logo/Logo'
 
 export default function Header() {
+  const router = useRouter()
+
+  function handleClick(direction: string) {
+    switch (direction) {
+      case 'upload':
+        router.push('/upload')
+        break
+      case 'signin':
+        router.push('/auth')
+        break
+    }
+  }
+
   return (
     <header className="header">
       <div id="header-top-right">
-        <button id="signup-button">Sign Up</button>
-        <button id="login-button">Log in</button>
+        <button id="upload-button" onClick={() => handleClick('upload')}>
+          Upload
+        </button>
+        <button id="signin-button" onClick={() => handleClick('signin')}>
+          Sign in
+        </button>
       </div>
-      <Link href="/">
-        <h1 id="fanemotions-logo">FanEmotions</h1>
-      </Link>
+      <Logo mode="black" />
       <SearchBar placeholder="Search for a FanEmotion" />
     </header>
   )
