3fe2c92bf195e7a8ee61d374263740b7d8a9fbfc
Benedict Wong <33556035+benedict-wong@users.noreply.github.com>
2026-09-16 15:39:09 -0700

    update eslint for ts/tsx react

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,30 +1,63 @@
-import js from '@eslint/js';
-import globals from 'globals';
-import json from '@eslint/json';
-import markdown from '@eslint/markdown';
-import css from '@eslint/css';
-import html from 'eslint-plugin-html';
-import { defineConfig } from 'eslint/config';
-import { createRequire } from 'module';
-const require = createRequire(import.meta.url);
-const prettierPlugin = require('eslint-plugin-prettier');
-const prettierConfig = require('./prettier.config.cjs');
+import storybook from 'eslint-plugin-storybook'
+import js from '@eslint/js'
+import globals from 'globals'
+import json from '@eslint/json'
+import markdown from '@eslint/markdown'
+import css from '@eslint/css'
+import html from 'eslint-plugin-html'
+import { defineConfig } from 'eslint/config'
+import tseslint from 'typescript-eslint'
+import { createRequire } from 'module'
+
+const require = createRequire(import.meta.url)
+const prettierPlugin = require('eslint-plugin-prettier')
+const prettierConfig = require('./prettier.config.cjs')
 
 export default defineConfig([
   {
     files: ['**/*.{js,mjs,cjs}'],
     plugins: { js, prettier: prettierPlugin },
     extends: ['js/recommended'],
-    languageOptions: { globals: globals.browser },
+    languageOptions: {
+      globals: globals.browser,
+    },
     rules: prettierConfig,
   },
-  { files: ['**/*.json'], plugins: { json }, language: 'json/json', extends: ['json/recommended'] },
+
+  {
+    files: ['**/*.{ts,tsx}'],
+    extends: [...tseslint.configs.recommended],
+    plugins: {
+      prettier: prettierPlugin,
+    },
+    rules: prettierConfig,
+  },
+
+  {
+    files: ['**/*.json'],
+    plugins: { json },
+    language: 'json/json',
+    extends: ['json/recommended'],
+  },
+
   {
     files: ['**/*.md'],
     plugins: { markdown },
     language: 'markdown/commonmark',
     extends: ['markdown/recommended'],
   },
-  { files: ['**/*.css'], plugins: { css }, language: 'css/css', extends: ['css/recommended'] },
-  { files: ['**/*.html'], plugins: { html } },
-]);
+
+  {
+    files: ['**/*.css'],
+    plugins: { css },
+    language: 'css/css',
+    extends: ['css/recommended'],
+  },
+
+  {
+    files: ['**/*.html'],
+    plugins: { html },
+  },
+
+  ...storybook.configs['flat/recommended'],
+])
