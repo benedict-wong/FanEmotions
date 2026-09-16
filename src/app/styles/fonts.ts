1122955f75659e5c0418a5bf88ff5a66801e1e85
Benedict Wong <33556035+benedict-wong@users.noreply.github.com>
2026-09-16 15:41:23 -0700

    add open sans and add font weights for roboto

--- src/app/styles/fonts.ts
+++ src/app/styles/fonts.ts
@@ -1,7 +1,7 @@
-import { Roboto, Source_Sans_3 } from 'next/font/google'
+import { Roboto, Source_Sans_3, Open_Sans } from 'next/font/google'
 export const roboto = Roboto({
   subsets: ['latin'],
-  weight: ['400', '700'],
+  weight: ['300', '400', '500', '700', '900'],
   variable: '--font-roboto',
 })
 
@@ -9,4 +9,10 @@ export const sourceSans = Source_Sans_3({
   subsets: ['latin'],
   weight: ['700'],
   variable: '--font-source-sans',
+})
+
+export const openSans = Open_Sans({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  variable: '--font-open-sans',
 })
