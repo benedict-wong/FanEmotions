16dac6332777e84d63e97642344ea9b5437c76b5
Benedict Wong <33556035+benedict-wong@users.noreply.github.com>
2026-09-16 17:09:32 -0700

    update imports for components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import Gallery from './components/Gallery'
-import Header from './components/Header'
+import Gallery from './components/Gallery/Gallery'
+import Header from './components/Header/Header'
 
 export default function Home() {
   return (
