f8c49e88a6582b865f9d07229339eef1536dd8fb
Benedict Wong <33556035+benedict-wong@users.noreply.github.com>
2026-09-18 13:35:04 -0700

    modify sign in component

--- src/app/components/Signin/Signin.tsx
+++ src/app/components/Signin/Signin.tsx
@@ -1,4 +1,6 @@
+import EmailInput from '../EmailInput/EmailInput'
 import PasswordInput from '../PasswordInput/PasswordInput'
+import Button from '../Button/Button'
 import styles from './signin.module.scss'
 
 export default function Signin() {
@@ -6,9 +8,13 @@ export default function Signin() {
     <div className={styles.container}>
       <h1 className={styles.title}>Welcome back!</h1>
       <form className={styles.form}>
-        <input type="email" id="signin-email-input" placeholder="Email"></input>
+        <EmailInput mode="signin" />
         <PasswordInput mode="signin" />
+        <Button className={styles.button}>Sign in</Button>
       </form>
+      <p className={styles.forgot}>
+        Forgot your <a href="#">username</a> or <a href="#">password</a>?
+      </p>
     </div>
   )
 }
