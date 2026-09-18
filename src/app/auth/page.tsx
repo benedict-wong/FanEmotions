2983dc9f3457be7cec6d4921dbc8ecb21de3caf2
Benedict Wong <33556035+benedict-wong@users.noreply.github.com>
2026-09-18 13:32:59 -0700

    change auth page to switch between sign in and register

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,15 +1,20 @@
+'use client'
+
 import AuthSwitch from '../components/AuthSwitch/AuthSwitch'
 import Signin from '../components/Signin/Signin'
+import Register from '../components/Register/Register'
 import Logo from '../components/Logo/Logo'
 import styles from './auth.module.scss'
+import { useState } from 'react'
 
-export default async function AuthPage() {
+export default function AuthPage() {
+  const [signinActive, setSigninActive] = useState(true)
   return (
     <div className={styles.container}>
-      <Logo mode="black" />
       <div className={styles['side-container']}>
-        <AuthSwitch />
-        <Signin />
+        <Logo mode="black" />
+        <AuthSwitch signinActive={signinActive} setSigninActive={setSigninActive} />
+        {signinActive ? <Signin /> : <Register />}
       </div>
     </div>
   )
